7970d3a98365fff8789dbd9c56f0e57ab0bfc461
13asTerem <you@example.com>
2025-05-22 17:57:09 +0200

    game class done

--- VideoGame/Game.cs
+++ VideoGame/Game.cs
@@ -8,7 +8,27 @@ namespace VideoGame
 {
     public class Game
     {
-     
+        public string GameID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public bool IsAvailable { get; set; }
+        public decimal PricePerDay { get; set; }
+
+        public Game(string gameID, string title, string genre, int releaseYear, decimal pricePerDay)
+        {
+            GameID = gameID;
+            Title = title;
+            Genre = genre;
+            ReleaseYear = releaseYear;
+            IsAvailable = true;
+            PricePerDay = pricePerDay;
+        }
+
+        public override string? ToString()
+        {
+            return $"{Title} ({ReleaseYear}) - {Genre} - {(IsAvailable ? "Elérhető" : "Kölcsönzött")} - {PricePerDay:C}/nap";
+        }
     }
 
 }
